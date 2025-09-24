de6a71b1c0273e0325fd4155355e1a6578d934fb
https://github.com/christiangeranvillahermosa-netizen/christian <christiangeran.villahermosa@hcdc.edu.ph>
2025-09-24 11:33:30 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "   / \\__      " << std::endl;
+    std::cout << "  (    @\\____ " << std::endl;
+    std::cout << "  /         O|" << std::endl;
+    std::cout << " /   (_____ / " << std::endl;
+    std::cout << "/_____ /   U  " << std::endl;
+    return 0;
+}
